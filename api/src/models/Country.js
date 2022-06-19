050d2f5658a8d98a9e91d86d52e1580a230ca2b9
mabhyHs <m4huens@gmail.com>
2022-06-18 21:31:58 -0300

    Country DB model

--- api/src/models/Country.js
+++ api/src/models/Country.js
@@ -1,12 +1,58 @@
-const { DataTypes } = require('sequelize');
+/*Model de País con las siguientes propiedades:
+ID (Código de 3 letras) *
+- Nombre *
+- Imagen de la bandera *
+- Continente *
+- Capital *
+- Subregión
+- Área
+- Población*/
+
+const { DataTypes } = require("sequelize");
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('country', {
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
+  sequelize.define(
+    "country",
+    {
+      id: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        primaryKey: true,
+        validate: {
+          len: 3,
+        },
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      flag: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      continent: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      capital: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      subregion: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      area: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+      },
+      population: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+      },
     },
-  });
+    { timestamp: false }
+  );
 };
