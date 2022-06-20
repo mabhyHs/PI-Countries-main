ee4c6d119dce00413280b3cddad994c150a4b25b
mabhyHs <m4huens@gmail.com>
2022-06-20 02:27:18 -0300

    trouble with brnachs fixed

--- client/src/index.js
+++ client/src/index.js
@@ -1,16 +1,3 @@
-<<<<<<< Updated upstream
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-=======
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
@@ -27,7 +14,6 @@ ReactDOM.render(
   </Provider>,
 
   document.getElementById("root")
->>>>>>> Stashed changes
 );
 
 // If you want to start measuring performance in your app, pass a function
