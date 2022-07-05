53e3ceb3e3f96a50752fbbd2c873db51cf38e34b
mabhyHs <m4huens@gmail.com>
2022-07-05 14:11:02 -0300

    finale commit

--- client/src/components/Country.jsx
+++ client/src/components/Country.jsx
@@ -6,14 +6,13 @@ export function Country({ id, name, continent, flag }) {
   return (
     <div className={styles.card} key={id}>
       <div className={styles.imgContainer}>
-      <Link className={styles.link} to={`/countries/${id}`}>
-
-        <img className={styles.img} src={flag} alt="national-flag" />
-      </Link>
+        <Link className={styles.link} to={`/countries/${id}`}>
+          <img className={styles.img} src={flag} alt="national-flag" />
+        </Link>
       </div>
       <div className={styles.textContainer}>
         {/* <Link className={styles.link} to={`/countries/${id}`}> */}
-          <h3 className={styles.country}>{name}</h3>
+        <h3 className={styles.country}>{name}</h3>
         {/* </Link> */}
         <h4 className={styles.continent}>{continent}</h4>
       </div>
