fcca1a4e03e10a06b17bef7d8678c27c2a7cdd20
mabhyHs <m4huens@gmail.com>
2022-07-06 20:04:20 -0300

    create redux folder and consTypes

--- client/src/Redux/store/index.js
+++ client/src/Redux/store/index.js
@@ -1,6 +1,6 @@
 import { createStore, applyMiddleware } from 'redux'
 import { composeWithDevTools } from 'redux-devtools-extension'  
-import rootReducer from '../reducers/index'
+import rootReducer from '../reducers'
 import thunkMiddleware from 'redux-thunk'
 
 
